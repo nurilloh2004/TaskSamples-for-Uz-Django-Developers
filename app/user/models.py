55959ed1f6e4b9f98f71738a90a804e2f350dd97
nurilloh2004 <nurillohpython@gmail.com>
2023-01-13 20:30:27 +0500

    all changed

--- app/user/models.py
+++ app/user/models.py
@@ -1,3 +1,31 @@
-from django.db import models
+"""
+Database models.
+"""
 
-# Create your models here.
+
+from django.db import models
+from django.conf import settings
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin,
+)
+from .manager import UserManager
+
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system."""
+    email = models.EmailField(max_length=255, unique=True)
+    username = models.CharField(max_length=50, blank=True, null=True)
+    phone = models.CharField(max_length=50, db_index=True, unique=True)
+    first_name = models.CharField(max_length=255, blank=True, null=True)
+    last_name = models.CharField(max_length=255, blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+
+
